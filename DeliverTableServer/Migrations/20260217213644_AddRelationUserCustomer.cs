df1a5d3327340c75fbad9d510d687e9a81f51d07
Valentin Neff <valentin.neff@ccicampus.fr>
2026-02-22 18:26:15 +0100

    feat(server): Ajout de identity pour gérer les sessions
    PBI: AB#5436
    
    Task: AB#555

---
+++ DeliverTableServer/Migrations/20260217213644_AddRelationUserCustomer.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DeliverTableServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationUserCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerProfiles_Users_UserId",
+                table: "CustomerProfiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CustomerProfiles_UserId",
+                table: "CustomerProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "CustomerProfiles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "CustomerProfiles",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerProfiles_Users_Id",
+                table: "CustomerProfiles",
+                column: "Id",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerProfiles_Users_Id",
+                table: "CustomerProfiles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "CustomerProfiles",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "CustomerProfiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerProfiles_UserId",
+                table: "CustomerProfiles",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerProfiles_Users_UserId",
+                table: "CustomerProfiles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
