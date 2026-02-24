fce8cafa62527aa18c220c3f63ecf6806cfd5b8a
Damien REICHHART <12345678+damienreichhart@users.noreply.github.com>
2026-02-24 23:37:43 +0100

    ix(server/client): correctifs code Rabbit
    PBI: AB#5560
    
    Task: AB#5561

--- DeliverTableSharedLibrary/Dtos/Auth/UserResponse.cs
+++ DeliverTableSharedLibrary/Dtos/Auth/UserResponse.cs
@@ -3,7 +3,7 @@
 public class UserResponse
 {
     public int Id {get; init;}
-    public string? Email { get; set; } = null;
+    public string Email { get; set; } = "";
     public string FirstName { get; set; } = "";
     public string LastName { get; set; } = "";
     public string Role { get; set; } = "";
