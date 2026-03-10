1f4528889acad4a42a69384ab2e743677bea5298
Valentin Neff <valentin.neff@ccicampus.fr>
2026-03-10 23:01:07 +0100

    feat(client): Add dish creation/update form
    PBI: AB#5452
    
    Task: AB#5648

--- DeliverTableSharedLibrary/Dtos/Dish/CreateDishDto.cs
+++ DeliverTableSharedLibrary/Dtos/Dish/CreateDishDto.cs
@@ -11,7 +11,6 @@ namespace DeliverTableSharedLibrary.Dtos.Dish
         [Required]
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Le prix doit être un nombre avec au plus 2 nombres après la virgule.")]
         [Range(0, 99999.99, ErrorMessage = "Le prix doit être entre 0 et 99999.99")]
         public decimal BasePrice { get; set; } = 0;
         public bool IsVegetarian { get; set; } = false;
