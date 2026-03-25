15d50afc73f32c0438dba2833ae2176ab4c75187
Damien REICHHART <12345678+damienreichhart@users.noreply.github.com>
2026-03-25 13:48:38 +0100

    feat(server): add RestaurantService.GetForMapAsync with tests
    PBI: AB#TODO
    Task: AB#TODO

--- DeliverTableServer/Services/Interfaces/IRestaurantService.cs
+++ DeliverTableServer/Services/Interfaces/IRestaurantService.cs
@@ -8,6 +8,7 @@ public interface IRestaurantService
 {
     Task<ServiceResult<PaginatedResult<RestaurantDto>>> GetAllAsync(RestaurantQuery query, CancellationToken ct = default);
     Task<ServiceResult<PaginatedResult<RestaurantDto>>> GetByOwnerAsync(int ownerId, RestaurantQuery query, CancellationToken ct = default);
+    Task<ServiceResult<List<RestaurantMapDto>>> GetForMapAsync(RestaurantQuery query, CancellationToken ct = default);
     Task<ServiceResult<DetailedRestaurantDto>> GetByIdAsync(int id, CancellationToken ct = default);
     Task<ServiceResult<RestaurantDto>> CreateAsync(CreateRestaurantDto dto, int ownerId, CancellationToken ct = default);
     Task<ServiceResult<DetailedRestaurantDto>> UpdateAsync(int id, UpdateRestaurantDto dto, CancellationToken ct = default);
