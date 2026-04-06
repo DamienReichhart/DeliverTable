16131cb0a2e28a03d184ac2f357b1a792931c58f
valentin <valentin.neff@ccicampus.fr>
2026-04-06 16:54:57 +0200

    rebase

--- DeliverTableServer/Controllers/ReclamationController.cs
+++ DeliverTableServer/Controllers/ReclamationController.cs
@@ -1,14 +1,13 @@
 ﻿using DeliverTableServer.Extensions;
 using DeliverTableServer.Services.Interfaces;
 using DeliverTableSharedLibrary.Constants;
-using DeliverTableSharedLibrary.Constants.Enums;
-using DeliverTableSharedLibrary.Dtos.Reclamation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using DeliverTableServer.Services.Interfaces;
 
 namespace DeliverTableServer.Controllers;
 
+
+// [Authorize]
 [ApiController]
 [Route(ApiRoutes.Reclamation.Base)]
 public class ReclamationController(
