578f82ceb35085a203b72fa12f9586019cf536f7
Damien REICHHART <12345678+damienreichhart@users.noreply.github.com>
2026-04-14 11:26:53 +0200

    rebase

---
+++ DeliverTableClient/Pages/Home/Home.razor.cs
@@ -0,0 +1,46 @@
+using DeliverTableSharedLibrary.Constants.Enums;
+using DeliverTableSharedLibrary.Dtos;
+using DeliverTableSharedLibrary.Constants;
+using DeliverTableClient.Services.Interfaces;
+
+namespace DeliverTableClient.Pages.Home
+{
+    public partial class Home(
+        IHealthApiClient healthApiClient
+    )
+    {
+        
+        private HealthResponse? _health = null;
+        private bool _isLoading = false;
+    
+        protected override async Task OnInitializedAsync()
+        {
+            _isLoading = true;
+            try
+            {
+                HealthResponse? response = await healthApiClient.GetHealthAsync();
+                _health = response;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            finally
+            {
+                _isLoading = false;
+            }
+        }
+    
+        private string DetermineColor()
+        {
+            Enum.TryParse<HealthStatus>(_health?.Status ?? HealthStatus.Unhealthy.ToString(), true, out HealthStatus status);
+            return status switch
+            {
+                HealthStatus.Healthy => "alert-success text-success",
+                HealthStatus.Degraded => "alert-warning  text-warning",
+                HealthStatus.Unhealthy => "alert-danger  text-danger",
+                _ => "alert-secondary"
+            };
+        }
+    }
+}
