12b2c6bc213ee8463901559f35cf67c48bde78d2
DamienReichhart <contact@damien-reichhart.fr>
2026-04-14 12:06:28 +0200

    style(shared): align new enum files with folder convention

--- DeliverTableSharedLibrary/Enums/LoyaltyRedemptionStatus.cs
+++ DeliverTableSharedLibrary/Enums/LoyaltyRedemptionStatus.cs
@@ -2,7 +2,7 @@ namespace DeliverTableSharedLibrary.Enums;
 
 public enum LoyaltyRedemptionStatus
 {
-    Pending = 0,
-    Committed = 1,
-    Reversed = 2,
+    Pending,
+    Committed,
+    Reversed,
 }
