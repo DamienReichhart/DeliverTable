d2e7d996a75938e90a6a29c8e1a0a25c96161f0f
DamienReichhart <contact@damien-reichhart.fr>
2026-04-14 14:48:27 +0200

    feat(shared): add VatRate, InvoiceKind, InvoiceIssuerType, InvoiceStatus enums

---
+++ DeliverTableSharedLibrary/Enums/InvoiceKind.cs
@@ -0,0 +1,9 @@
+namespace DeliverTableSharedLibrary.Enums;
+
+public enum InvoiceKind
+{
+    OrderInvoiceToCustomer,
+    CommissionInvoiceToRestaurant,
+    CreditNoteToCustomer,
+    CommissionCreditNoteToRestaurant,
+}
