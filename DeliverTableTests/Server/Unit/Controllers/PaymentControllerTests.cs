4656253a11988b521fecae01b0bcb6d68bcb0a22
DamienReichhart <contact@damien-reichhart.fr>
2026-04-14 13:08:23 +0200

    feat(server): add PaymentController cancel endpoint with tests

---
+++ DeliverTableTests/Server/Unit/Controllers/PaymentControllerTests.cs
@@ -0,0 +1,48 @@
+using DeliverTableServer.Common;
+using DeliverTableServer.Controllers;
+using DeliverTableServer.Services.Interfaces;
+using DeliverTableSharedLibrary.Constants.Enums;
+using DeliverTableTests.Global.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DeliverTableTests.Server.Unit.Controllers;
+
+[TestFixture]
+public class PaymentControllerTests
+{
+    private IPaymentService _service = null!;
+    private PaymentController _sut = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _service = Substitute.For<IPaymentService>();
+        _sut = new PaymentController(_service);
+    }
+
+    [Test]
+    public async Task Cancel_Authenticated_ReturnsNoContent()
+    {
+        AuthenticationTestHelper.SetupAuthenticatedUser(_sut, "7", nameof(UserRole.Customer));
+        _service.CancelAuthorizationAsync(42, Arg.Any<CancellationToken>())
+                .Returns(ServiceResult.Success());
+
+        var result = await _sut.Cancel(42, CancellationToken.None);
+
+        Assert.That(result, Is.InstanceOf<NoContentResult>());
+    }
+
+    [Test]
+    public async Task Cancel_ServiceError_ReturnsError()
+    {
+        AuthenticationTestHelper.SetupAuthenticatedUser(_sut, "7", nameof(UserRole.Customer));
+        _service.CancelAuthorizationAsync(42, Arg.Any<CancellationToken>())
+                .Returns(ServiceResult.Failure(new ServiceError("Annulation impossible", 400)));
+
+        var result = await _sut.Cancel(42, CancellationToken.None);
+
+        Assert.That(result, Is.Not.InstanceOf<NoContentResult>());
+    }
+}
