6c65ec067d1573a5b97ea4b8d9e287690e33599c
DamienReichhart <contact@damien-reichhart.fr>
2026-04-15 08:26:28 +0200

    feat(db): add migration AddDisputes

--- DeliverTableInfrastructure/Migrations/20260415062539_AddDisputes.Designer.cs
+++ DeliverTableInfrastructure/Migrations/20260415062539_AddDisputes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DeliverTableInfrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DeliverTableInfrastructure.Migrations
 {
     [DbContext(typeof(DeliverTableContext))]
-    partial class DeliverTableContextModelSnapshot : ModelSnapshot
+    [Migration("20260415062539_AddDisputes")]
+    partial class AddDisputes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
