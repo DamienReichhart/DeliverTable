5c20ef4258af9b5cc18f2b448be276fd522a5277
Damien REICHHART <12345678+damienreichhart@users.noreply.github.com>
2026-05-04 10:45:58 +0200

    feat(db): add migration AddUserBillingAddress

---
+++ DeliverTableInfrastructure/Migrations/20260504084520_AddUserBillingAddress.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliverTableInfrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserBillingAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BillingAddressLine1",
+                table: "AspNetUsers",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingAddressLine2",
+                table: "AspNetUsers",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingCity",
+                table: "AspNetUsers",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingCountry",
+                table: "AspNetUsers",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingPostalCode",
+                table: "AspNetUsers",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BillingAddressLine1",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingAddressLine2",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingCity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingCountry",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingPostalCode",
+                table: "AspNetUsers");
+        }
+    }
+}
