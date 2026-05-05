49263cb30d063f7cff019ef8d3dba59712672c82
Damien REICHHART <12345678+damienreichhart@users.noreply.github.com>
2026-05-05 09:13:09 +0200

    feat(client/server): Live dishes interface implementation
    PBI: AB#5967
    
    Task: AB#5968

--- DeliverTableSharedLibrary/Dtos/Order/OrderQuery.cs
+++ DeliverTableSharedLibrary/Dtos/Order/OrderQuery.cs
@@ -3,6 +3,9 @@ namespace DeliverTableSharedLibrary.Dtos.Order;
 public class OrderQuery
 {
     public string? Status { get; set; }
+    public bool? ToPrepare { get; set; }
+    public string? SortBy { get; set; }
+    public bool? SortDesc { get; set; }
     public int PageNumber { get; set; } = 1;
     public int PageSize { get; set; } = 20;
 }
