956c3eb9a522f8fa5fa93422c7582bcd574d09a8
DamienReichhart <contact@damien-reichhart.fr>
2026-05-29 09:33:42 +0200

    feat(server): add commission statement repository interface
    PBI: AB#5994
    Task: AB#6012

---
+++ DeliverTableInfrastructure/Repositories/Interfaces/ICommissionStatementRepository.cs
@@ -0,0 +1,25 @@
+using DeliverTableInfrastructure.Models;
+using DeliverTableSharedLibrary.Enums;
+
+namespace DeliverTableInfrastructure.Repositories.Interfaces;
+
+public interface ICommissionStatementRepository
+{
+    Task<CommissionStatement> CreateAsync(CommissionStatement statement, CancellationToken ct = default);
+    Task UpdateAsync(CommissionStatement statement, CancellationToken ct = default);
+
+    Task<CommissionStatement?> GetByIdAsync(int id, CancellationToken ct = default);
+    Task<CommissionStatement?> GetByIdWithLinesAndRecipientAsync(int id, CancellationToken ct = default);
+
+    Task<bool> InvoiceExistsForPeriodAsync(int restaurantId, int year, int month, CancellationToken ct = default);
+
+    Task<List<int>> ListRestaurantIdsWithEligibleOrdersAsync(
+        DateTime periodStartUtc, DateTime periodEndUtc, CancellationToken ct = default);
+
+    Task<List<Order>> ListEligibleOrdersForRestaurantAsync(
+        int restaurantId, DateTime periodStartUtc, DateTime periodEndUtc, CancellationToken ct = default);
+
+    Task<CommissionStatementLine?> FindLineByRefundEventIdAsync(string refundEventId, CancellationToken ct = default);
+
+    Task<int> AllocateNextNumberAsync(CancellationToken ct = default);
+}
