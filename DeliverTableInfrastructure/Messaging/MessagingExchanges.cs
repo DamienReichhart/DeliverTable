8fa692dfe1a525866d3cf13ee5deee0167ac306a
DamienReichhart <contact@damien-reichhart.fr>
2026-05-29 09:18:43 +0200

    feat(server): add commission statement messaging routing key
    PBI: AB#5994
    Task: AB#6012

--- DeliverTableInfrastructure/Messaging/MessagingExchanges.cs
+++ DeliverTableInfrastructure/Messaging/MessagingExchanges.cs
@@ -4,4 +4,5 @@ public static class MessagingExchanges
 {
     public const string Email = "email";
     public const string Invoice = "invoice";
+    public const string CommissionStatement = "commission_statement";
 }
