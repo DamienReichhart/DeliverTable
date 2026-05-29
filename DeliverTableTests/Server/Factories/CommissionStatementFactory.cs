b1d3176fa83f5bf9966068fe475e9be60f1f115c
Damien REICHHART <12345678+damienreichhart@users.noreply.github.com>
2026-05-29 09:37:15 +0200

    test(server): add commission statement repository tests
    PBI: AB#5994
    Task: AB#6012

---
+++ DeliverTableTests/Server/Factories/CommissionStatementFactory.cs
@@ -0,0 +1,25 @@
+using DeliverTableInfrastructure.Models;
+using DeliverTableSharedLibrary.Enums;
+
+namespace DeliverTableTests.Server.Factories;
+
+public static class CommissionStatementFactory
+{
+    public static CommissionStatement CreateInvoice(
+        int restaurantId,
+        int year,
+        int month,
+        string number = "COMM-2026-05-000001") => new()
+    {
+        Number = number,
+        Kind = CommissionStatementKind.Invoice,
+        RecipientRestaurantId = restaurantId,
+        PeriodYear = year,
+        PeriodMonth = month,
+        IssuedAt = DateTime.UtcNow,
+        Currency = "EUR",
+        Status = CommissionStatementStatus.Queued,
+        IssuerLegalSnapshotJson = "{}",
+        RecipientSnapshotJson = "{}",
+    };
+}
