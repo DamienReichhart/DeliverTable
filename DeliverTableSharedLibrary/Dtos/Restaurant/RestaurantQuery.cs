7bb283079cab799e9d91a9b1cec6f7964f375640
valentin <valentin.neff@ccicampus.fr>
2026-06-14 18:27:01 +0200

    fix(client/server): fix problem post rebase

--- DeliverTableSharedLibrary/Dtos/Restaurant/RestaurantQuery.cs
+++ DeliverTableSharedLibrary/Dtos/Restaurant/RestaurantQuery.cs
@@ -8,6 +8,7 @@ public class RestaurantQuery
     public double? Latitude { get; set; }
     public double? Longitude { get; set; }
     public double? RadiusKm { get; set; }
+    public bool IsActive { get; set; } = true;
     public int PageNumber { get; set; } = 1;
     public int PageSize { get; set; } = 20;
 }
